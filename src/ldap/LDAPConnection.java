a7b2f070bf776b9b33e0193f8a6acafde40903f5
Kevin <kounvonglasy_kevin@hotmail.fr>
2016-04-09 21:01:21 +0200

    Ajout LDAP

---
+++ src/ldap/LDAPConnection.java
@@ -0,0 +1,54 @@
+package ldap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import beans.Utilisateur;
+
+class ConnexionForm
+{
+	private static final String CHAMP_LOGIN = "username";
+	private static final String CHAMP_PASS = "password";
+	
+	public Utilisateur connecterUtilisateur(HttpServletRequest request) {
+		LDAPaccess access = new LDAPaccess();
+		
+		/* Récupération des champs du formulaire */
+		String login = getValeurChamp(request, CHAMP_LOGIN);
+		String mdp = getValeurChamp(request, CHAMP_PASS);
+		
+		try {
+		LDAPObject connexion = access.LDAPget(login, mdp);
+		if (connexion == null)
+		{
+			System.out.println("login invalide");
+			System.exit(1);
+		}
+		System.exit(0);
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+			System.exit(1);
+		}
+	    //Si la connexion s'est bien passé,retourne le bean
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.setLogin(login);
+		return utilisateur;
+		
+	}
+	
+	/*
+	 * 
+	 * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
+	 * 
+	 * sinon.
+	 * 
+	 */
+	private static String getValeurChamp(HttpServletRequest request, String nomChamp) {
+		String valeur = request.getParameter(nomChamp);
+		
+		if (valeur == null || valeur.trim().length() == 0) {
+			return null;
+		} else {
+			return valeur;
+		}
+	}
+}
