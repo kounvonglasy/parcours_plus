957cf3d74e1b8bc742124fdc76efe335307713bf
Kevin <kevin.raharison@isep.fr>
2016-04-10 13:29:46 +0200

    Modifications
    mineure

---
+++ P+/Vue/HTML/head.php
@@ -0,0 +1,9 @@
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>Mon profil</title>
+		<link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="../CSS/style.css" rel="stylesheet">
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
